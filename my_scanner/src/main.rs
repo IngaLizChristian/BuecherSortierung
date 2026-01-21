454fb5b187ceeb292c9c1b2ee0752adf693e312c
IngaLizChristian <141242811+IngaLizChristian@users.noreply.github.com>
2026-01-21 21:06:04 +0100

    forschleife incoming

--- my_scanner/src/main.rs
+++ my_scanner/src/main.rs
@@ -1,19 +1,40 @@
 use std::io::{self, BufRead};
 
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct TestingSerde {
+    items: Vec<Item>,
+}
+#[derive(Deserialize, Debug)]
+struct Item {
+    volumeInfo: VolumeInfo,
+}
+#[derive(Deserialize, Debug)]
+struct VolumeInfo {
+    title: String,
+    authors: Vec<String>,
+    publishedDate: String,
+}
+
 fn main() {
-    println!("ISBN einlesen \n");
-    let stdin = io::stdin();
-    let isbn = stdin.lock().lines().next().unwrap().unwrap();
+    println!("ISBN einlesen");
 
-    let url = format!(
-        "https://www.googleapis.com/books/v1/volumes?q=isbn:{}",
-        isbn
-    );
-    let response_text = reqwest::blocking::get(url)
-        .expect("couldnt make request")
-        .text()
-        .expect("could not read");
+    for isbn in io::stdin().lock().lines() {
+        let isbn = isbn.unwrap();
+        if isbn == "X001RPRVCZ" || isbn == "X001RPRVCY" {
+            break;
+        }
+        let url = format!("https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}");
 
-    println!("\n");
-    println!("ResponseText: {}", response_text);
+        let response_text = reqwest::blocking::get(url)
+            .expect("couldnt make request")
+            .text()
+            .expect("could not read");
+
+        let response: TestingSerde = serde_json::from_str(&response_text).unwrap();
+
+        dbg!(response_text);
+        println!("{response:?}");
+    }
 }
