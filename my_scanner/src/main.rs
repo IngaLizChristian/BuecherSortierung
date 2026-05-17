b0ce126544bf797f319154c0633905fa6b66e016
IngaLizChristian <141242811+IngaLizChristian@users.noreply.github.com>
2026-05-17 19:18:37 +0200

    called the tabels into existence
    made the tabels and then commented the part where I create them to not always create them anew

--- my_scanner/src/main.rs
+++ my_scanner/src/main.rs
@@ -9,24 +9,24 @@ struct TestingSerde {
 }
 #[derive(Deserialize, Debug)]
 struct Item {
-    volumeInfo: VolumeInfo,
+    volume_info: VolumeInfo,
 }
 #[derive(Deserialize, Debug)]
 struct VolumeInfo {
     title: String,
     author: Vec<String>,
-    publishedDate: String,
+    published_date: String,
 }
 
 fn main() {
     //Öffnen der connection zur DB
     let conn = Connection::open("bibDB.db").unwrap();
+    /*
+        //-------------------------------------------AUTOREN_TABELLEN----------------------------------------------------------------
 
-    //-------------------------------------------AUTOREN-TABELLEN----------------------------------------------------------------
-
-    //Erstellen der Autorentabelle
-    conn.execute(
-        "CREATE TABLE author (
+        //Erstellen der Autorentabelle
+        conn.execute(
+            "CREATE TABLE author (
             author_id               INTEGER NOT NULL,
 
             author_first_name       VARCHAR(255),
@@ -38,68 +38,68 @@ fn main() {
             PRIMARY KEY (author_id),
             UNIQUE(author_id)
         )",
-        (), // empty list of parameters.
-    )
-    .unwrap();
+            (), // empty list of parameters.
+        )
+        .unwrap();
 
-    //erstellen der "Wer hat was geschrieben" - Tabelle, reine JOIN tabelle
-    conn.execute(
-        "CREATE TABLE wrote (
+        //erstellen der "Wer hat was geschrieben" - Tabelle, reine JOIN tabelle
+        conn.execute(
+            "CREATE TABLE wrote (
             author_id           INTEGER,
             isbn                INTEGER,
 
             FOREIGN KEY(author_id)  REFERENCES author(author_id),
             FOREIGN KEY(isbn)       REFERENCES book(isbn)
         )",
-        (), // empty list of parameters.
-    )
-    .unwrap();
+            (), // empty list of parameters.
+        )
+        .unwrap();
 
-    //-------------------------------------------BÜCHER_TABELLEN----------------------------------------------------------------
+        //-------------------------------------------BÜCHER_TABELLEN----------------------------------------------------------------
 
-    //----------------------------WICHTIGE HINWEISE ZU DEN BÜCHERN----------------------------
-    /*
-        Bild einfügen
-            INSERT INTO books (cover_image_name, cover_image_path)
-            VALUES ('logo', LOAD_FILE('/pfad/zum/bild.png'));
+        //----------------------------WICHTIGE HINWEISE ZU DEN BÜCHERN----------------------------
+        /*
+            Bild einfügen
+                INSERT INTO books (cover_image_name, cover_image_path)
+                VALUES ('logo', LOAD_FILE('/pfad/zum/bild.png'));
 
-        Bild auslesen
-            SELECT cover_image_name, cover_image_path FROM books WHERE isbn = XXXXX;
+            Bild auslesen
+                SELECT cover_image_name, cover_image_path FROM books WHERE isbn = XXXXX;
 
-        Bild anzeigen, bsp php
-            echo '<img src="' . $row['cover_image_path'] . '" alt="Titelbild">';
+            Bild anzeigen, bsp php
+                echo '<img src="' . $row['cover_image_path'] . '" alt="Titelbild">';
 
-        -------------------------------------------
+            -------------------------------------------
 
-        liked_rating goes from 1-10, like a star system
+            liked_rating goes from 1-10, like a star system
 
-        -------------------------------------------
+            -------------------------------------------
 
-        reading_state exampels: - currently reading
-                                - read
-                                - to be read
-                                - didn't finish
+            reading_state exampels: - currently reading
+                                    - read
+                                    - to be read
+                                    - didn't finish
 
-        -------------------------------------------
+            -------------------------------------------
 
-        owned_state exampels:   - owned
-                                - borrowed
-                                - lend
-                                - wishlist
-                                - sold
-                                - lost
-                                - gifted
+            owned_state exampels:   - owned
+                                    - borrowed
+                                    - lend
+                                    - wishlist
+                                    - sold
+                                    - lost
+                                    - gifted
 
-        -------------------------------------------
+            -------------------------------------------
 
-        mature_rating can be set individually, for example if it can be read by small children, read by small children under supervision, read by teenagers without any problems,
-        is only for adults, mostly will be used as a yes no thingy, but is if it has specific limitations by whom it should be read
-    */
-    //----------------------------------------------------------------------------------------
+            mature_rating can be set individually, for example if it can be read by small children, read by small children under supervision, read by teenagers without any problems,
+            is only for adults, mostly will be used as a yes no thingy, but is if it has specific limitations by whom it should be read
+        */
+        //----------------------------------------------------------------------------------------
 
-    //Erstellen der Büchertabelle
-    conn.execute(
-        "CREATE TABLE book (
+        //Erstellen der Büchertabelle
+        conn.execute(
+            "CREATE TABLE book (
             isbn                    INTEGER NOT NULL,
             copy_number             INTEGER NOT NULL,
 
@@ -125,13 +125,13 @@ fn main() {
             UNIQUE(isbn),
             UNIQUE(copy_number)
         )",
-        (), // empty list of parameters.
-    )
-    .unwrap();
+            (), // empty list of parameters.
+        )
+        .unwrap();
 
-    //Erstellen der has_tag Tabelle
-    conn.execute(
-        "CREATE TABLE has_tag (
+        //Erstellen der has_tag Tabelle
+        conn.execute(
+            "CREATE TABLE has_tag (
             isbn                    INTEGER,
             copy_number             INTEGER,
             tag_id                  INTEGER,
@@ -140,26 +140,26 @@ fn main() {
             FOREIGN KEY(copy_number)    REFERENCES book(copy_number),
             FOREIGN KEY(tag_id)         REFERENCES tag(tag_id)
         )",
-        (), // empty list of parameters.
-    )
-    .unwrap();
+            (), // empty list of parameters.
+        )
+        .unwrap();
 
-    //Erstellen der tag Tabelle
-    conn.execute(
-        "CREATE TABLE tag (
+        //Erstellen der tag Tabelle
+        conn.execute(
+            "CREATE TABLE tag (
             tag_id                 INTEGER NOT NULL,
             tag_name               VARCHAR(255),
 
             PRIMARY KEY(tag_id),
             UNIQUE(tag_id)
         )",
-        (), // empty list of parameters.
-    )
-    .unwrap();
+            (), // empty list of parameters.
+        )
+        .unwrap();
 
-    //Erstellen der has_genre Tabelle
-    conn.execute(
-        "CREATE TABLE has_genre (
+        //Erstellen der has_genre Tabelle
+        conn.execute(
+            "CREATE TABLE has_genre (
             isbn                    INTEGER,
             genre_id                INTEGER,
             primary_genre           BOOLEAN,
@@ -167,38 +167,38 @@ fn main() {
             FOREIGN KEY(isbn)               REFERENCES book(isbn),
             FOREIGN KEY(genre_id)           REFERENCES genre(genre_id)
         )",
-        (), // empty list of parameters.
-    )
-    .unwrap();
+            (), // empty list of parameters.
+        )
+        .unwrap();
 
-    //Erstellen der genre Tabelle
-    //---------------------------------
-    /*
-        Genre-Verschachtelung Beispiele:
+        //Erstellen der genre Tabelle
+        //---------------------------------
+        /*
+            Genre-Verschachtelung Beispiele:
 
-        Horror  - Psycho - Horror
-                - hack'n'slash
-                - kosmischer - Horror
-        Möglichst nie tiefer als 2 Ebenen runter gehen, maybe durch ein dropdown festsetzen?
-    */
-    //---------------------------------
-    conn.execute(
-        "CREATE TABLE genre (
+            Horror  - Psycho - Horror
+                    - hack'n'slash
+                    - kosmischer - Horror
+            Möglichst nie tiefer als 2 Ebenen runter gehen, maybe durch ein dropdown festsetzen?
+        */
+        //---------------------------------
+        conn.execute(
+            "CREATE TABLE genre (
             genre_id                INTEGER NOT NULL,
             parent_genre_id         INTEGER,
             genre_name              VARCHAR(255),
             
             PRIMARY KEY(genre_id),
             UNIQUE(genre_id),
-            FOREIN KEY(parent_genre_id)     REFERENCES genre(genre_id)
+            FOREIGN KEY(parent_genre_id)     REFERENCES genre(genre_id)
         )",
-        (), // empty list of parameters.
-    )
-    .unwrap();
+            (), // empty list of parameters.
+        )
+        .unwrap();
 
-    //Erstellen der is_part_of Tabelle
-    conn.execute(
-        "CREATE TABLE is_part_of (
+        //Erstellen der is_part_of Tabelle
+        conn.execute(
+            "CREATE TABLE is_part_of (
             isbn                    INTEGER,
             series_id               INTEGER,
 
@@ -208,25 +208,25 @@ fn main() {
             FOREIGN KEY(isbn)               REFERENCES book(isbn),
             FOREIGN KEY(series_id)          REFERENCES series(series_id)
         )",
-        (), // empty list of parameters.
-    )
-    .unwrap();
+            (), // empty list of parameters.
+        )
+        .unwrap();
 
-    //Erstellen der series Tabelle
-    conn.execute(
-        "CREATE TABLE series (
+        //Erstellen der series Tabelle
+        conn.execute(
+            "CREATE TABLE series (
             series_id               INTEGER NOT NULL,
 
             series_titel            VARCHAR(255),
             series_description      TEXT,
 
             PRIMARY KEY(series_id),
-            UNIQUE(series_id),
+            UNIQUE(series_id)
         )",
-        (), // empty list of parameters.
-    )
-    .unwrap();
-
+            (), // empty list of parameters.
+        )
+        .unwrap();
+    */
     //------------------------------------------------------------------------ENDE ERSTELLUNG DER DATENBANKEN------------------------------------------------------------------------
     //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     println!("ISBN einlesen");
@@ -254,9 +254,9 @@ fn main() {
         conn.execute(
             "INSERT INTO books (titel, author, publishedDate) VALUES (?1, ?2, ?3)",
             (
-                &response.items[0].volumeInfo.title,
-                &response.items[0].volumeInfo.author[0],
-                &response.items[0].volumeInfo.publishedDate,
+                &response.items[0].volume_info.title,
+                &response.items[0].volume_info.author[0],
+                &response.items[0].volume_info.published_date,
             ),
         )
         .unwrap();
